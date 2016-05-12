6199026ba08621ff0a0db3703448db0b7f8f2ebc
Konstantin Baierer <unixprog@gmail.com>
2016-05-12 02:05:18 +0200

    bin: -L option to list, more robust saxon call

--- bin/ocr-transform.sh
+++ bin/ocr-transform.sh
@@ -7,11 +7,16 @@ source "$SHAREDIR/lib.sh"
 show_usage() {
     echo "Usage: $0 [-dh] <input-fmt> <output-fmt> [<input> [<output>]] [-- <saxon_opts>]"
 }
+SAXON_ARGS=()
 
-while getopts ":dh" opt; do
+while getopts ":dhL" opt; do
     case "$opt" in
         d)
             let DEBUG+=1
+            ;;
+        L)
+            echo "${XSLT_SCRIPTS[@]}"
+            exit 0
             ;;
         h)
             show_usage
@@ -46,16 +51,19 @@ if ! containsElement "$to" "${XSLT_OUT[@]}";then
     show_output_formats
     exit 1
 fi
-SAXON_ARGS+=("-xsl:$SHAREDIR/${from}__${to}.xsl")
+SAXON_ARGS+=("-xsl:$SHAREDIR/xslt/${from}__${to}.xsl")
 shift 2
 
 # input
 if [[ -z "$1" ]];then
-    echo "Reading from STDIN" >&2
+    echo_err "Reading from STDIN"
     SAXON_ARGS+=('-s:-')
 elif [[ "$1" == '--' ]];then
     shift
-    SAXON_ARGS+=($@)
+    while [[ ! -z "$1" ]];do
+        SAXON_ARGS+=("$1")
+        shift
+    done
 else
     SAXON_ARGS+=("-s:$1")
     shift;
@@ -64,18 +72,24 @@ fi
 # output
 if [[ "$1" == '--' ]];then
     shift
-    SAXON_ARGS+=($@)
+    while [[ ! -z "$1" ]];do
+        SAXON_ARGS+=("$1")
+        shift
+    done
 elif [[ ! -z "$1" ]];then
     SAXON_ARGS+=("-o:$1")
     shift;
 else
-    echo "Writing to STDOUT" >&2
+    echo_err "Writing to STDOUT"
 fi
 
 # saxon_opts
 if [[ "$1" == '--' ]];then
     shift
-    SAXON_ARGS+=($@)
+    while [[ ! -z "$1" ]];do
+        SAXON_ARGS+=("$1")
+        shift
+    done
 fi
 
 # Run it
