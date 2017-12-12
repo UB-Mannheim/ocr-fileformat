7bbe16773d6a35db47785c8059a1f22ae37a7c70
Stefan Weil <sw@weilnetz.de>
2017-12-12 12:13:47 +0100

    Remove unused global statement
    This fixes a warning from Codacy.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- web/ocr-fileformat.php
+++ web/ocr-fileformat.php
@@ -30,7 +30,6 @@ function sendJSON($data)
  */
 function pipeToCommand($cmd, $xml)
 {
-  global $config;
   $errfile = "/tmp/error-output.txt";
   $descriptorspec = array(
     0 => array("pipe", "r"),
